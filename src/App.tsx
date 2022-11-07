db2af09c96975723b351d80797557264c10e70ac
horvath12.gabor <horvath12.gabor@ext.t-systems.hu>
2022-11-07 21:57:31 +0100

    create a simple frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {MouseEvent} from 'react';
+import {SketchPicker} from 'react-color';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+
+class App extends React.Component<any,any> {
+  constructor(props:any) {
+    super(props);
+
+    this.state = {
+      color: {
+        r: 100,
+        g: 100,
+        b: 100,
+        a: 1
+      },
+      effect: ""
+    }
+
+    this.handleColorChange = this.handleColorChange.bind(this);
+    this.handleSetColor = this.handleSetColor.bind(this);
+  }
+
+  handleColorChange(newColor: any) {
+    this.setState({color: newColor.rgb});
+  }
+
+  handleSetColor(event: MouseEvent) {
+
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className='main'>
+        <h1>Choose a color</h1>
+        <SketchPicker color={this.state.color} onChange={this.handleColorChange} />
+        <button className='button' type='button' onClick={this.handleSetColor}>Set Color</button>
+        <h1>Chose an effect</h1>
+        <div className="fade" style={{backgroundColor: `rgba(${this.state.color.r},${this.state.color.g},${this.state.color.b},${this.state.color.a})`}}></div>
+        <div className="pulse" style={{backgroundColor: `rgba(${this.state.color.r},${this.state.color.g},${this.state.color.b},${this.state.color.a})`}}></div>
+        <div className="rainbow"></div>
+      </div>
+    )
+  }
 }
 
 export default App;
